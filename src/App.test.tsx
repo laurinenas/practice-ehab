54ad2224bcce3fd7dce30d1cac7cb7c4164fc10b
Andrius Laurinenas <andrius@instaffo.de>
2022-01-16 18:39:07 +0100

    Add test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test("renders button group with a default value", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/1 day/i);
   expect(linkElement).toBeInTheDocument();
 });
